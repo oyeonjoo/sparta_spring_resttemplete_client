1406f2bae64be43d8e6620f2b683c589842f7d54
oyeonjoo <oyj0842@naver.com>
2024-02-17 02:11:38 +0900

    RestTemplate getForEntity 요청

--- src/main/java/com/sparta/springresttemplateclient/SpringRestTemplateClientApplication.java
+++ src/main/java/com/sparta/springresttemplateclient/SpringRestTemplateClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringResttemplateClientApplication {
+public class SpringRestTemplateClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringResttemplateClientApplication.class, args);
+		SpringApplication.run(SpringRestTemplateClientApplication.class, args);
 	}
 
 }
